ba18f8e749a20ae2a33c446af7557a216eb4f564
Vikas-coding <vikascodingpractice@gmail.com>
2023-09-04 12:08:53 +0530

    changes in portfolio

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const htmlProgressBar = document.querySelector('.progress-html .progress__fill');
+const cssProgressBar = document.querySelector('.progress-css .progress__fill');
+const javascriptProgressBar = document.querySelector('.progress-javascript .progress__fill');
+
+// Update the progress bars as needed
+htmlProgressBar.style.width = '75%';
+cssProgressBar.style.width = '90%';
+javascriptProgressBar.style.width = '60%';
